8e74ff5a74bdeb8b723809b5eba47e438aeeb6b3
Vijay K <sharmavijay86@gmail.com>
2022-08-25 23:08:26 +0530

    class

--- gke-tf/network.tf
+++ gke-tf/network.tf
@@ -48,3 +48,26 @@ resource "google_dns_managed_zone" "private-zone" {
     }
   }
 }
+// cloud router setup for private gke node
+resource "google_compute_router" "router" {
+  name    = "gke-rt-${random_id.randhex.hex}"
+  region  = var.region
+  network = google_compute_network.gke_vpc.id
+
+  bgp {
+    asn = 64514
+  }
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "gke-rt-nat-${random_id.randhex.hex}"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
+}
